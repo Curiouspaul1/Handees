c4220a9e5a80f09af0305d1ed4027205a90a753e
Curiouspaul1 <170408025@live.unilag.edu.ng>
2021-07-22 02:32:30 +0100

    finished configuring app

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,22 @@
-#!python3
+from flask import Flask
+from config import config
+from .extensions import (
+    db, migrate
+)
+import os
 
+def create_app(config_name):
+    # create flask app instance
+    app = Flask(__name__)
+    
+    # set config using config name
+    app.config.from_object(config[config_name])
+
+    # register extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    # register blueprints
+    # from .user import 
+
+    return app
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,17 @@
+from core import create_app
+import os
 
+# create server instance from app factory
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
+
+
+# configuring flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        app=app
+    )
+
+
+if __name__ == "__main__":
+    app.run()
